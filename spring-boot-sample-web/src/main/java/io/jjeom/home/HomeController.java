2b4352c42c58a4c6844bf3ae27557546ad295685
jeongjaeeom <jeongjae.eom@gmail.com>
2019-01-02 10:23:37 +0900

    Fix

--- spring-boot-sample-web/src/main/java/io/jjeom/home/HomeController.java
+++ spring-boot-sample-web/src/main/java/io/jjeom/home/HomeController.java
@@ -2,14 +2,13 @@ package io.jjeom.home;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author Jeongjae Eom
  * @since 2018-12-28
  */
 @Controller
-@RequestMapping({"", "/"})
+//@RequestMapping({"", "/"})
 public class HomeController {
 
     @GetMapping
