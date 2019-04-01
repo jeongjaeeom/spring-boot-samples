eab9858535520db0a0a3eb5da81c9dc881d5cd13
jeongjaeeom <jeongjae.eom@gmail.com>
2019-04-01 23:04:50 +0900

    Add

--- rest-api/src/main/java/org/jjeom/restapi/events/Event.java
+++ rest-api/src/main/java/org/jjeom/restapi/events/Event.java
@@ -1,8 +1,36 @@
 package org.jjeom.restapi.events;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
 /**
  * @author eomjeongjae
  * @since 2019-04-01
  */
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+//@Setter
+@EqualsAndHashCode(of = "id")
 public class Event {
+    private Integer id;
+    private String name;
+    private String description;
+    private LocalDateTime beginEnrollmentDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
+    private LocalDateTime beginEventDateTime;
+    private LocalDateTime endEventDateTime;
+    private String location;
+    private int basePrice;
+    private int maxPrice;
+    private int limitOfEnrollment;
+    private boolean offline;
+    private boolean free;
+    private EventStatus eventStatus;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
---
+++ rest-api/src/main/java/org/jjeom/restapi/events/EventStatus.java
@@ -0,0 +1,9 @@
+package org.jjeom.restapi.events;
+
+/**
+ * @author eomjeongjae
+ * @since 2019-04-01
+ */
+public enum EventStatus {
+    DRAFT, PUBLISHED, BEGAN_ENROLLMENT;
+}
