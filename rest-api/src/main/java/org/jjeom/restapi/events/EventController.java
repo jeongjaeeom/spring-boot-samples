fdbdef4f955d596a0590f80281134fa3e44fb717
jeongjaeeom <jeongjae.eom@gmail.com>
2019-05-10 12:26:58 +0900

    Add

--- rest-api/src/main/java/org/jjeom/restapi/events/EventController.java
+++ rest-api/src/main/java/org/jjeom/restapi/events/EventController.java
@@ -5,7 +5,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,9 +26,15 @@ public class EventController {
     
     private final EventRepository eventRepository;
     private final ModelMapper modelMapper;
+    private final EventValidator eventValidator;
     
     @PostMapping
     public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        eventValidator.validate(eventDto, errors);
         if (errors.hasErrors()) {
             return ResponseEntity.badRequest().build();
         }
---
+++ rest-api/src/main/java/org/jjeom/restapi/events/EventValidator.java
@@ -0,0 +1,31 @@
+package org.jjeom.restapi.events;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author eomjeongjae
+ * @since 2019-05-10
+ */
+@Component
+public class EventValidator {
+
+    public void validate(EventDto eventDto, Errors errors) {
+        if (eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() > 0) {
+            errors.reject("wrongPrices", "Values fo prices are wrong");
+        }
+
+        LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
+        if (endEventDateTime.isBefore(eventDto.getBeginEventDateTime()) ||
+                endEventDateTime.isBefore(eventDto.getCloseEnrollmentDateTime()) ||
+                endEventDateTime.isBefore(eventDto.getBeginEnrollmentDateTime())) {
+            errors.rejectValue("endEventDateTime", "wrongValue", "endEventDateTime is wrong");
+        }
+
+        // TODO BeginEventDateTime
+        // TODO CloseEnrollmentDateTime
+    }
+
+}
